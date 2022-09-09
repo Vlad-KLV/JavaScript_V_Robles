2f9eb1ad6b2c204d5b135bc6a67efae23c19d608
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2022-09-09 22:31:28 +0200

    .

--- EjerciciosDeArraysFunciones/array.js
+++ EjerciciosDeArraysFunciones/array.js
@@ -13,12 +13,6 @@ var numeros = new Array(6);
 
 
 /*Para meterlo de nuevo por pantalla ordenado,lo mas recomendable es hacer una funcion. Esta funcion va recibir un parametro (elementos)y un (textoCustom ="") para pasarle un dato sin interes. (elementos) seria el array y va sustituit  numero*/ 
-function mostrarArray(elementos,textoCustom = ""){
-    document.write("<h2>Contenido del Array" + textoCustom + " : </h2>");
-    elementos.forEach((elemento, index) => {
-        document.write("<h3><strong>" + elemento + "</strong></br></h3>");
-    });
-}
 
 
 
@@ -28,6 +22,7 @@ for (var i = 0; i <= 5; i++) {
     numeros.push(parseInt(prompt("Introduce un numero", 0)));
 }
 //MOSTRAR EN EL CUERPO DE LA PAGINA
+
 mostrarArray(numeros);
 /* esta parte la llevo para la function mostrarArray.
 document.write("<h2>Contenido del Array:</h2>");
@@ -41,8 +36,32 @@ numeros.forEach((numero, index) => {
 console.log(numeros);
 
 
+function mostrarArray(elementos,textoCustom = ""){
+    document.write("<h2>Contenido del Array" + textoCustom + " : </h2>");
+    elementos.forEach((elemento, index) => {
+        document.write("<h3><strong>" + elemento + "</strong></br></h3>");
+    });
+}
+
+
 //ORDENAR Y MOSTRAR:es un orden alfavetico.
+
 numeros.sort();
 //console.log(numeros);
-//cuando yo quiero volver a mostrar ordenado la llamo popr aki:
-mostrarArray(numeros, "ordenado")
+//cuando yo quiero volver a mostrar ordenado(orden alfavetico) la llamo por aki:
+mostrarArray(numeros, " ordenado alfavetico");
+//para orden numerico descendente
+numeros.sort(function(a,b){return b-a});
+mostrarArray(numeros, " ordenado descendente ")
+//para orden numerico ascendente
+
+numeros.sort(function(a,b){return a-b});
+mostrarArray(numeros, " ordenado ascendente");
+
+//INVERTIR Y MOSTRAR.
+numeros.reverse();
+mostrarArray(numeros, " ordenado inverso");
+
+//CONTAR ELEMENTO
+document.write("<h4>El array tiene " +numeros.length +" elementos</h4>");
+
