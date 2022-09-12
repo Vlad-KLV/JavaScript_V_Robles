58a4338965ae33de55050d79112dd70da06765c9
Vlad-KLV <107071633+Vlad-KLV@users.noreply.github.com>
2022-09-12 09:51:02 +0200

    Update callback.js
    Reordenado y re-estilizado según el convenio de js.  https://standardjs.com/rules.html
    Introducido el check de edad en la función mayorDeEdad().
    Comentada la variable <nombre> dentro del check, la función nunca llega a recibir su valor.

--- funciones/callback/callback.js
+++ funciones/callback/callback.js
@@ -1,33 +1,37 @@
 "use strict"
-function saludar(nombre) { 
-    alert('Hola ' + nombre); 
-    } 
-
-    function procesarEntradaUsuario(callback) { 
-    var nombre = prompt('como te llamas NneNO.'); 
-    callback(nombre); 
-    } 
-
-    procesarEntradaUsuario(saludar); 
 
 
-function mayorDeEdad(edad){
-    alert("Hola , usted tiene"  + edad );
-}
-
-function procesarEdadUsuario(callback) { 
-var edad = parseInt( prompt('cuantos años tienes',0)); 
-callback(edad); 
-} 
-
+procesarEntradaUsuario(saludar);
 procesarEdadUsuario(mayorDeEdad);
 
-/* no funcciona señor
-poniendo saludar y mayorDeEdad  tmb propisyvaet chto popalo.
-if(mayorDeEdad>=18){
-    //si es mayor de edad
-    document.write(nombre+ " tiene"+edad+" años y es mayor de edad");
-}else{
-    // y si es menor de edad
-    document.write( nombre +" tiene"+edad+" años y es menor de edad");
-}*/
+
+
+function procesarEntradaUsuario(callback) {
+    var nombre = prompt('como te llamas NneNO.');
+    callback(nombre);
+}
+
+
+function saludar(nombre) {
+    alert('Hola ' + nombre);
+}
+
+
+function procesarEdadUsuario(callback) {
+    var edad = parseInt(prompt('cuantos años tienes', 0));
+    callback(edad);
+}
+
+
+
+function mayorDeEdad(edad) {
+    alert("Hola , usted tiene" + edad);
+    if (edad >= 18) {
+        //la función no recibe el valor de var <nombre> y no es capaz de procesar
+        document.write( /*nombre+ */ " tiene" + edad + " años y es mayor de edad");
+    } else {
+        //la función no recibe el valor de var <nombre> y no es capaz de procesar
+        document.write( /*  nombre +*/  " tiene" + edad + " años y es menor de edad");
+    }
+}
+
