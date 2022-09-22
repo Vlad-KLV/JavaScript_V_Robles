8188249c91c4e933e11528ee413a1be7f8042d43
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2022-09-22 23:51:04 +0200

    +

--- bucleWhile/bucleWhile.js
+++ bucleWhile/bucleWhile.js
@@ -15,11 +15,46 @@ while( contador < 10){
 }
 
 
-
+/*
 let contador2 = 5;
 while(contador<15){
     contador++;
     console.log("Bucle:",contador);
     if(contador==13)break;
+  
     
 }
+  
+let color = "" 
+while (color != "rojo"){ 
+   	color = prompt("dame un color (escribe rojo para salir)","") 
+}
+let colorDo 
+do { 
+   	colorDo = prompt("dame un color (escribe rojo para salir)","") 
+} while (colorDo != "verde")
+
+
+let numero =""
+//while(numero = 13)
+while(numero < 13){
+    numero=parseInt(prompt("introduce un numero",0));
+  // numero= prompt("Introduce un numero:");
+}
+
+
+var suma = 0 
+while (suma < 1000){ 
+   	suma += parseInt(Math.random() * 100) 
+   	document.write (suma + " /") 
+}
+
+let i = parseInt(prompt("Introduce un numero:",0));
+do {
+    i +=1;
+    document.write(i);
+    console.log(i);
+ } while (i < 5);ç
+ ***********************/
+ 
+
