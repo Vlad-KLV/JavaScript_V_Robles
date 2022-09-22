adf800896cea34a286a7cc87898c8314535309c1
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2022-09-22 06:34:46 +0200

    .

--- bucleWhile/bucleWhile.js
+++ bucleWhile/bucleWhile.js
@@ -1,6 +1,25 @@
 "use strict"
+/*
 let eyar = 2018;
 while(eyar<=2051){
     console.log("estamos en el ano:" +eyar );
     eyar++;
+}
+************************************************/
+let contador = 0;
+while( contador < 10){
+    contador++;
+   
+    if(contador == 5)break;
+    console.log(" El bucle va por el numero:" , contador);
+}
+
+
+
+let contador2 = 5;
+while(contador<15){
+    contador++;
+    console.log("Bucle:",contador);
+    if(contador==13)break;
+    
 }
